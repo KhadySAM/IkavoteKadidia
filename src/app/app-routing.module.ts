2abc65b407e27d74a189e1337cf1cf0f0b9b372c
Kadidiatou SAMAKE <ksamake18@gmail.com>
2023-01-28 19:15:07 +0000

    two

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { PersonnelsComponent } from './Pages/personnels/personnels.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  {
+    path:'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path:'personnel',
+    component: PersonnelsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { PersonnelsComponent } from './Pages/personnels/personnels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PersonnelsComponent
   ],
   imports: [
     BrowserModule,
